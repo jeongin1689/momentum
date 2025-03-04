239cc2519d5d6eeabc300535f9a7ab2c04a863c9
박정인 <jeongin@hivelab.co.kr>
2025-03-04 17:07:01 +0900

    Feat: clock js

---
+++ js/clock.js
@@ -0,0 +1,19 @@
+const clock = document.querySelector(".js-clock .clock__text");
+
+function getTime() {
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const second = now.getSeconds();
+  const time = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${second}`;
+  clock.innerHTML = time;
+  return;
+}
+
+function init() {
+  getTime();
+  setInterval(getTime, 1000);
+  return;
+}
+
+init();
