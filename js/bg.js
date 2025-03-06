d1c7d643087d710f3fa1e34acc8dad424d491d10
박정인 <jeongin@hivelab.co.kr>
2025-03-06 11:42:20 +0900

    Fix: API key 따로 관리

--- js/bg.js
+++ js/bg.js
@@ -1,5 +1,6 @@
-const UNSPLASH_API_KEY =
-  "b491e86a6957b396f44f1e15e41d3d242e17aa982607f161b95defd195c7f4dd";
+import config from "./config.js";
+const { UNSPLASH_API_KEY } = config;
+
 const UNSPLASH_URL = `https://api.unsplash.com/photos/random/?client_id=${UNSPLASH_API_KEY}&query=landscape&orientation=landscape`;
 
 const body = document.querySelector("body")
