d0e084f5cecd85eee42d2d40d7c678eafd4666cc
박정인 <jeongin@hivelab.co.kr>
2025-03-07 17:15:14 +0900

    Fix: location 추가

--- js/weather.js
+++ js/weather.js
@@ -4,6 +4,7 @@ const { WEATHERSTACK_API_KEY } = config;
 const WEATHERSTACK_URL = `https://api.weatherstack.com/current?access_key=${WEATHERSTACK_API_KEY}&query="Seoul"`;
 const weatherText = document.querySelector(".js-weather .weather_text");
 const weatherImg = document.querySelector(".js-weather .weather_icon img");
+const weatherCountryText = document.querySelector(".js-location .location_text");
 
 function getWeather() {
   fetch(WEATHERSTACK_URL)
@@ -14,8 +15,12 @@ function getWeather() {
       if (weather) {
         const weatherDescription = weather.current.weather_descriptions;
         const weatherIcon = weather.current.weather_icons;
+        const weatherCountry = weather.location.country;
+        const weatherLocation = weather.location.name;
+
         weatherText.innerHTML = weatherDescription;
         weatherImg.src = weatherIcon;
+        weatherCountryText.innerHTML = weatherCountry +'/'+ weatherLocation;
       }
     });
   return;
