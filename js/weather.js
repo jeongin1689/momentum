ffa72d98909e75c6b47230ccdc1de0cd06670f81
박정인 <jeongin@hivelab.co.kr>
2025-03-07 15:43:17 +0900

    Fix: 날씨 정보 입력

--- js/weather.js
+++ js/weather.js
@@ -2,17 +2,22 @@ import config from "./config.js";
 const { WEATHERSTACK_API_KEY } = config;
 
 const WEATHERSTACK_URL = `https://api.weatherstack.com/current?access_key=${WEATHERSTACK_API_KEY}&query="Seoul"`;
+const weatherText = document.querySelector(".js-weather .weather_text");
+const weatherImg = document.querySelector(".js-weather .weather_icon img");
 
-function getBackground() {
-    fetch(WEATHERSTACK_URL)
-      .then(response => response.json())
-      .then(json => {
-        const weather = json;
-        console.log(weather)
-        if (weather) {
-          const weatherCurrent = weather.current;
-        }
-      });
-    return;
-  }
-  getBackground();
+function getWeather() {
+  fetch(WEATHERSTACK_URL)
+    .then(response => response.json())
+    .then(json => {
+      const weather = json;
+      console.log(weather)
+      if (weather) {
+        const weatherDescription = weather.current.weather_descriptions;
+        const weatherIcon = weather.current.weather_icons;
+        weatherText.innerHTML = weatherDescription;
+        weatherImg.src = weatherIcon;
+      }
+    });
+  return;
+}
+getWeather();
